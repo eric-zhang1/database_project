2f90583f0dadca8bfb67602c7dbec77da31638af
eric-zhang1 <ericletianzhang@gmail.com>
2024-11-07 17:27:34 -0500

    Connected to the database

--- Main.java
+++ Main.java
@@ -1,7 +1,35 @@
 import Classes.Menu;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
+
+    private static String DATABASE = "Database.db";
+    public static Connection conn = null;
+
+    public static Connection initializeDatabase(String path) {
+        String url = "jdbc:sqlite:" + path;
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url);
+            if (connection != null) {
+                DatabaseMetaData metaData = connection.getMetaData();
+                System.out.println("Using driver: " + metaData.getDriverName());
+                System.out.println("Database connected successfully.");
+            } else {
+                System.out.println("Null Connection");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("There was a problem connecting to the database.");
+        }
+        return connection;
+    }
+
     public static void main(String[] args) {
+        conn = initializeDatabase(DATABASE);
         Menu menu = new Menu();  // Instantiate the application
         while (true) {
             int choice = menu.getNextOption();  // Get the option wanted from the customer
